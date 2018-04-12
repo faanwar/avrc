199299289994e59a00709ee40fb79b6e87331b1a
Fakhira Anwar <faanwar@ucsd.edu>
2018-04-12 11:10:11 -0700

    fixing indentation error

--- aeh.earlytest/src/aeh/earlytest/results.py
+++ aeh.earlytest/src/aeh/earlytest/results.py
@@ -174,6 +174,7 @@ def get_patient(rc_id):
         except:
           #log.info(traceback.format_exc())
     except:
+      pass
 	  #log.info(traceback.format_exc())
 
 def find_phone_email(clean_rcid):
