c05ffcb4bcfb34d44aa2244c7007959c6b363480
Fakhira Anwar <faanwar@ucsd.edu>
2018-04-18 13:33:48 -0700

    a

--- avrc.redcross/src/avrc/redcross/sync_redcap.py
+++ avrc.redcross/src/avrc/redcross/sync_redcap.py
@@ -46,7 +46,9 @@ def get_results(redcap):
     results = []
     try:
         all_records = redcap.project['CTS'].export_records(fields=['rc_id', 'nat_results_complete'])       
-        filtered_records = (x['rc_id'] for x in all_records if x['nat_results_complete'] == '2')    
+        filtered_records = (x['rc_id'] for x in all_records if x['nat_results_complete'] == '2')   
+        print 'fileter' 
+        print filtered_records
         unprocessed_records = (x['rc_id'] for x in filtered_records if  x['rec_status'] != '1')    
         records = redcap.project['CTS'].export_records(records=unprocessed_records)
 
