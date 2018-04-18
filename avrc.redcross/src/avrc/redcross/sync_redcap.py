5a9dd50d9b1e88f733195517fb150ed87e28a1c6
Fakhira Anwar <faanwar@ucsd.edu>
2018-04-18 13:23:40 -0700

    a

--- avrc.redcross/src/avrc/redcross/sync_redcap.py
+++ avrc.redcross/src/avrc/redcross/sync_redcap.py
@@ -46,10 +46,12 @@ def get_results(redcap):
     results = []
     try:
         all_records = redcap.project['CTS'].export_records(fields=['rc_id', 'nat_results_complete'])       
-        filtered_records = (x['rc_id'] for x in all_records if x['nat_results_complete'] == '2' )     
+        filtered_records = (x['rc_id'] for x in all_records if x['nat_results_complete'] == '2' and x['rec_status'] != '1')     
         records = redcap.project['CTS'].export_records(records=filtered_records)
 
-        for record in records:           
+        for record in records:   
+            print 'record'
+            print record        
             rcid= record['rc_id']
             result = models.Result(
                 site_code=rcid[:4],
