67313b3a0c4397874208e3445ce1886444ddc767
Fakhira Anwar <faanwar@ucsd.edu>
2018-04-22 21:45:11 -0700

    k

--- avrc.redcross/src/avrc/redcross/sync_redcap.py
+++ avrc.redcross/src/avrc/redcross/sync_redcap.py
@@ -47,13 +47,13 @@ def get_results(redcap):
         print 'x'
         print x
         a = x['nat_results_complete'] == '2' and x['rec_status'] != '1'
+        print 'a'
         print a
         return a
 
     results = []
     try:
-        all_records = redcap.project['CTS'].export_records(fields=['rc_id', 'nat_results_complete', 'rec_status'])  
-        print all_records     
+        all_records = redcap.project['CTS'].export_records(fields=['rc_id', 'nat_results_complete', 'rec_status'])     
         filtered_records = (x['rc_id'] for x in all_records if is_criteria_met(x) == 'true')   
 
         records = redcap.project['CTS'].export_records(records=filtered_records)
