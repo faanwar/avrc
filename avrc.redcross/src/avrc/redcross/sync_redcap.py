378d6d8ec90edccfa22d6415b31ce10ed4c6d56a
Fakhira Anwar <faanwar@ucsd.edu>
2018-04-22 22:35:41 -0700

    a

--- avrc.redcross/src/avrc/redcross/sync_redcap.py
+++ avrc.redcross/src/avrc/redcross/sync_redcap.py
@@ -54,7 +54,8 @@ def get_results(redcap):
         all_records = redcap.project['CTS'].export_records(fields=['rc_id', 'nat_results_complete', 'rec_status'])     
         filtered_records = (x['rc_id'] for x in all_records if is_criteria_met(x) == True) 
         print 'f done'
-      
+        print 'length'
+        print len(filtered_records) 
         if(len(filtered_records) > 0):
             records = redcap.project['CTS'].export_records(records=filtered_records)
         else:
