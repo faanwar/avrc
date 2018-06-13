50345bf5aa3de7c5b38f4090fce6a387cac629f5
Fakhira Anwar <faanwar@ucsd.edu>
2018-06-13 14:51:15 -0700

    a

--- avrc.redcross/src/avrc/redcross/scripts/sync.py
+++ avrc.redcross/src/avrc/redcross/scripts/sync.py
@@ -149,11 +149,12 @@ def sync_sql_result(buckets, settings):
         # Visit/Draw date update in SQL DB
         if 'visit_date' in record.keys() and record['visit_date'] != '':
           visit_date =  dt.strptime(record['visit_date'], "%Y-%m-%d").date() 
-          if sql_row.draw_date != visit_date:
-            if sql_row.site_code == 'SDPT':
+          if sql_row.site_code == 'SDPT':
               print 'dates not equal'
               print sql_row.draw_date
               print visit_date
+          if sql_row.draw_date != visit_date:
+            
             if sql_row.test_date >= visit_date:
               print 'update visit date'
               sql_row.draw_date = visit_date
